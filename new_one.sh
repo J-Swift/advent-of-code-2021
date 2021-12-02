4361ea5ed2c2167c9684ecac1836a8ceebbc5e87
Jimmy Reichley <jimmyqpublik@gmail.com>
2021-12-02 10:18:05 -0500

    Add new_one.sh

---
+++ new_one.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -p bash -i bash
+
+set -euo pipefail
+
+readonly day="${1:-}"
+
+if [ -z "${day}" ];  then
+    echo "ERROR: day not provided"
+    echo
+    echo "USAGE: ${0} [day #]"
+    exit 1
+fi
+
+main() {
+    local part
+    if [ -d "day_${day}/part_01" ]; then
+        local part="02"
+        cp -r "day_${day}/part_01" "day_${day}/part_02"
+    else
+        local part="01"
+        local -r path="day_${day}/part_01"
+        mkdir -p "${path}"
+        touch "${path}/input_test.txt"
+        touch "${path}/input.txt"
+        cat << EOF  > "${path}/run_it.ts"
+async function loadInput() {
+    return await Deno.readTextFile("./input_test.txt");
+}
+
+function parseInput(input: string) {
+    return input.split("\n").map(it => +it);
+}
+
+async function main() {
+    const input = parseInput(await loadInput());
+}
+
+main()
+EOF
+    fi
+
+    code "day_${day}/part_${part}/run_it.ts"
+}
+
+main
