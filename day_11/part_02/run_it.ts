de6e26f7a9474a4bf02eb61952efe638eb1a74bc
Jimmy Reichley <jimmyqpublik@gmail.com>
2021-12-11 10:56:08 -0500

    Day 11

---
+++ day_11/part_02/run_it.ts
@@ -0,0 +1,89 @@
+async function loadInput() {
+    return await Deno.readTextFile("./input.txt")
+}
+
+function parseInput(input: string) {
+    return input.split("\n").map(it => it.split("").map(Number))
+}
+
+function increment(grid: number[][]) {
+    return grid.map(line => line.map(it => it+1))
+}
+
+function getKey(row: number, col: number) {
+    return `[${row}][${col}]`
+}
+
+function doFlash(grid: number[][], row: number, col: number) {
+    for (let colIdx=col-1; colIdx<=col+1; colIdx++) {
+        if (0 <= colIdx && colIdx < grid.length) {
+            for (let rowIdx=row-1; rowIdx<=row+1; rowIdx++) {
+                if (0 <= rowIdx && rowIdx < grid[0].length) {
+                    if (!(rowIdx == row && colIdx == col)) {
+                        grid[colIdx][rowIdx]++
+                    }
+                }
+            }
+        }
+    }
+}
+
+function resolveFlashes(grid: number[][], flashed = new Set<string>()) {
+    let startingFlashes = flashed.size
+
+    do {
+        startingFlashes = flashed.size
+        grid.forEach((line, colIdx) => {
+            line.forEach((cell, rowIdx) => {
+                let key = getKey(rowIdx, colIdx)
+                if (cell > 9 && !flashed.has(key)) {
+                    flashed.add(key)
+                    doFlash(grid, rowIdx, colIdx)
+                }
+            })
+        })
+    } while(startingFlashes!=flashed.size)
+
+    grid.forEach((line, colIdx) => {
+        line.forEach((cell, rowIdx) => {
+            if (cell > 9) {
+                grid[colIdx][rowIdx] = 0
+            }
+        })
+    })
+
+    return {grid, flashes: flashed.size}
+}
+
+function tick(grid: number[][]) {
+    let flashes = 0
+
+    grid = increment(grid)
+    return resolveFlashes(grid)
+}
+
+function display(g: {grid: number[][], flashes: number}) {
+    console.log('--------------------')
+    console.log(g.flashes)
+    console.log()
+    g.grid.forEach(line => {
+        console.log(line.join(""))
+    })
+}
+
+async function main() {
+    const input = parseInput(await loadInput())
+
+    let t1 = {grid: input, flashes: 0}
+    let total = 0
+    let keepGoing = true
+    let numCells = t1.grid.length * t1.grid[0].length
+    while (keepGoing) {
+        total++
+        t1 = tick(t1.grid)
+        keepGoing = t1.flashes != numCells
+    }
+    console.log(`TOTAL TICKS: [${total}]`)
+}
+
+main()
